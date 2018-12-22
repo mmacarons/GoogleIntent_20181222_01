4284e149181212785ce3d7c8c414ef70b1e44c17
manymany.macarons <manymany.macarons@gmail.com>
2018-12-22 11:14:54 +0900

    SMS 발송 기능 연습

--- app/src/main/java/kr/tjit/googleintent_20181222_01/MainActivity.java
+++ app/src/main/java/kr/tjit/googleintent_20181222_01/MainActivity.java
@@ -1,24 +1,79 @@
 package kr.tjit.googleintent_20181222_01;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends BaseActivity {
+
+    EditText phoneNumEdt;
+    Button dialBtn;
+    Button callBtn;
+    EditText contentEdt;
+    Button smsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bindViews();
         setupEvents();
         setValues();
-        bindViews();
 
     }
 
     @Override
     public void setupEvents() {
 
+        dialBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                폰번 에딧텍스트의 입력값을 받고
+                String inputPhoneNum = phoneNumEdt.getText().toString();
+
+//                그 입력값을 전화를 거는데 사용
+
+                Uri uri = Uri.parse(String.format("tel:%s", inputPhoneNum));
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                사용자가 입력한 폰번을 받아서 저장
+                String inputPhoneNum = phoneNumEdt.getText().toString();
+
+                Uri uri = Uri.parse(String.format("tel:%s", inputPhoneNum));
+
+                Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                startActivity(intent);
+            }
+        });
+
+        smsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                사용자가 입력한 폰번을 저장
+                String inputNumber = phoneNumEdt.getText().toString();
+
+//                사용자가 입력한 내용을 저장
+                String inputContent = contentEdt.getText().toString();
+
+//                SMS 발송
+                Uri uri = Uri.parse(String.format("smsto:%s", inputNumber));
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra("sms_body", inputContent);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -28,6 +83,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+
+        dialBtn = findViewById(R.id.dialBtn);
+        phoneNumEdt = findViewById(R.id.phoneNumEdt);
+        callBtn = findViewById(R.id.callBtn);
+        contentEdt = findViewById(R.id.contentEdt);
+        smsBtn = findViewById(R.id.smsBtn);
 
     }
 }
